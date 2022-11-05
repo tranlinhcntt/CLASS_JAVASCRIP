2ad55566c7047a37b38c0153cb7a60d52198b9d8
tranlinhcntt <tranlinhcntt@gmail.com>
2022-11-05 16:34:39 +0700

    var java

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// khai bao bien
+var weight= 60;
+var fullname='Tran Duy linh';
+var age = 32;
+
+// alert(weight)
+// alert(age)
+// alert(fullname)
+
+/**
+ * khai baos bieen
+ * cach vo hieu hoa ma
+ * 
+ */
